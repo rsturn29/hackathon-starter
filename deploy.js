a28b49c568bf09b338ad96c8af592f25bbe46192
Beck <rsturn29@gmail.com>
2022-08-11 05:20:44 -0500

    change to deploy.js

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.88.241.69',
+      host: '100.24.6.202',
       username: 'ubuntu',
       privateKeyPath: 'labsuser.pem'
     })
